2e4c98f5a5dbf1dfcb50accfe336f62e09c4b3f1
Ismail-Iyada <ismailiyada@gmail.com>
2024-05-18 20:57:05 +0100

    ch-13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,63 +17,85 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  };
+  try {
+    const rawFormData = {
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    };
 
-  //   !! better way, when theres a lot of formdata
-  const test = Object.fromEntries(formData.entries());
-  console.log(rawFormData);
-  //   !! console.log('test', test);
+    //   !! better way, when theres a lot of formdata
+    const test = Object.fromEntries(formData.entries());
+    console.log(rawFormData);
+    //   !! console.log('test', test);
 
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
 
-  //   console.log(
-  //     typeof rawFormData.amount,
-  //     typeof amount,
-  //     typeof customerId,
-  //     typeof status,
-  //   );
+    //   console.log(
+    //     typeof rawFormData.amount,
+    //     typeof amount,
+    //     typeof customerId,
+    //     typeof status,
+    //   );
 
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
-  // console.log(customerId, amount, status, amountInCents, date);
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    // console.log(customerId, amount, status, amountInCents, date);
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to create Invoice.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+  try {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
 
-  const amountInCents = amount * 100;
+    const amountInCents = amount * 100;
 
-  await sql`
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    console.log(error);
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
+
+// ! Note how redirect is being called outside of the try/catch block. This is because redirect works by throwing an error, which would be caught by the catch block. To avoid this, you can call redirect after try/catch. redirect would only be reachable if try is successful.
